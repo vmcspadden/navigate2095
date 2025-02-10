8ad0f19a9eddd37ae354c9b2f7bc17023ab5f35e
Annie Wang <annie.xd.wang@gmail.com>
2025-02-10 11:50:16 -0600

    update ome tiff schema path

--- src/navigate/model/metadata_sources/ome_tiff_metadata.py
+++ src/navigate/model/metadata_sources/ome_tiff_metadata.py
@@ -85,7 +85,7 @@ class OMETIFFMetadata(XMLMetadata):
             "xmlns": "http://www.openmicroscopy.org/Schemas/OME/2016-06",
             "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
             "xsi:schemaLocation": "http://www.openmicroscopy.org/Schemas/OME/2016-06 "
-            "http://www.openmicroscopy.org/Schemas/OME/2016-06/ome.xsd",
+            "https://www.openmicroscopy.org/Schemas/OME/2016-06/ome.xsd",
         }
         if uid is not None:
             # Assume uid is a list passed in the order of the channels
